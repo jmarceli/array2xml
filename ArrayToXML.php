e67846d6aac9f79a40f0a3a45103a85b77977e10
Jan Grzegorowski <grzegorowski@gmail.com>
2014-06-28 16:04:17 +0200

    rewrite with more objective approach (remove static functions), change assoc array detection in order to treat numeric assoc arrays same as sequential arrays

--- ArrayToXML.php
+++ ArrayToXML.php
@@ -10,7 +10,7 @@ class ArrayToXML {
    * @return string XML String containig values
    * @return mixed Boolean false on failure, string XML result on success
    */
-  public static function buildXML($data, $startElement = 'data', $xml_version = '1.0', $xml_encoding = 'UTF-8'){
+  public function buildXML($data, $startElement = 'data', $xml_version = '1.0', $xml_encoding = 'UTF-8'){
     if(!is_array($data)){
       $err = 'Invalid variable type supplied, expected array not found on line '.__LINE__." in Class: ".__CLASS__." Method: ".__METHOD__;
       trigger_error($err);
@@ -22,76 +22,81 @@ class ArrayToXML {
     $xml->startDocument($xml_version, $xml_encoding);
     $xml->startElement($startElement);
 
-    /**
-     * Write keys in $data prefixed with @ as XML attributes, if $data is an array. 
-     * When an @ prefixed key is found, a '%' key is expected to indicate the element itself, 
-     * and '#' prefixed key indicates CDATA content
-     *
-     * @param object $xml XMLWriter Object
-     * @param array $data with attributes filtered out
-     */
-    function writeAttr(XMLWriter $xml, $data) {
-      if(is_array($data)) {
-        $nonAttributes = array();
-        foreach($data as $key => $val) {
-          //handle an attribute with elements
-          if($key[0] == '@') {
-            $xml->writeAttribute(substr($key, 1), $val);
-          } else if($key[0] == '%') {
-            if(is_array($val)) $nonAttributes = $val;
-            else $xml->text($val);
-          } elseif($key[0] == '#') {
-            if(is_array($val)) $nonAttributes = $val;
-            else {
-              $xml->startElement(substr($key, 1));
-              $xml->writeCData($val);
-              $xml->endElement();
-            }
-          }
-
-          //ignore normal elements
-          else $nonAttributes[$key] = $val;
-        }
-        return $nonAttributes;
-      }
-      else return $data;
-    }
-
-    /**
-     * Write XML as per Associative Array
-     * @param object $xml XMLWriter Object
-     * @param array $data Associative Data Array
-     */
-    function writeEl(XMLWriter $xml, $data) {
-      foreach($data as $key => $value) {
-        if(is_array($value) && isset($value[0])) { //numeric array
-          foreach($value as $itemValue){
-            if(is_array($itemValue)) {
-              $xml->startElement($key);
-              $itemValue = writeAttr($xml, $itemValue);
-              writeEl($xml, $itemValue);
-              $xml->endElement();
-            } else {
-              $itemValue = writeAttr($xml, $itemValue);
-              $xml->writeElement($key, "$itemValue");
-            }
-          }
-        } else if(is_array($value)) { //associative array
-          $xml->startElement($key);
-          $value = writeAttr($xml, $value);
-          writeEl($xml, $value);
-          $xml->endElement();
-        } else { //scalar
-          $value = writeAttr($xml, $value);
-          $xml->writeElement($key, "$value");
-        }
-      }
-    }
-    writeEl($xml, $data);
+    $this->writeEl($xml, $data);
 
     $xml->endElement();//write end element
     //returns the XML results
     return $xml->outputMemory(true);
+  }
+  /**
+   * Write keys in $data prefixed with @ as XML attributes, if $data is an array. 
+   * When an @ prefixed key is found, a '%' key is expected to indicate the element itself, 
+   * and '#' prefixed key indicates CDATA content
+   *
+   * @param object $xml XMLWriter Object
+   * @param array $data with attributes filtered out
+   */
+  protected function writeAttr(XMLWriter $xml, $data) {
+    if(is_array($data)) {
+      $nonAttributes = array();
+      foreach($data as $key => $val) {
+        //handle an attribute with elements
+        if($key[0] == '@') {
+          $xml->writeAttribute(substr($key, 1), $val);
+        } else if($key[0] == '%') {
+          if(is_array($val)) $nonAttributes = $val;
+          else $xml->text($val);
+        } elseif($key[0] == '#') {
+          if(is_array($val)) $nonAttributes = $val;
+          else {
+            $xml->startElement(substr($key, 1));
+            $xml->writeCData($val);
+            $xml->endElement();
+          }
+        }
+
+        //ignore normal elements
+        else $nonAttributes[$key] = $val;
+      }
+      return $nonAttributes;
+    }
+    else return $data;
+  }
+
+  /**
+   * Write XML as per Associative Array
+   * @param object $xml XMLWriter Object
+   * @param array $data Associative Data Array
+   */
+  protected function writeEl(XMLWriter $xml, $data) {
+    foreach($data as $key => $value) {
+      if(is_array($value) && !$this->isAssoc($value)) { //numeric array
+        foreach($value as $itemValue){
+          if(is_array($itemValue)) {
+            $xml->startElement($key);
+            $itemValue = $this->writeAttr($xml, $itemValue);
+            $this->writeEl($xml, $itemValue);
+            $xml->endElement();
+          } else {
+            $itemValue = $this->writeAttr($xml, $itemValue);
+            $xml->writeElement($key, "$itemValue");
+          }
+        }
+      } else if(is_array($value)) { //associative array
+        $xml->startElement($key);
+        $value = $this->writeAttr($xml, $value);
+        $this->writeEl($xml, $value);
+        $xml->endElement();
+      } else { //scalar
+        $value = $this->writeAttr($xml, $value);
+        $xml->writeElement($key, "$value");
+      }
+    }
+  }
+
+  // Checks if array is an associative
+  protected function isAssoc($array) {
+    return (bool)count(array_filter(array_keys($array), 'is_string'));
   }
 }
  
